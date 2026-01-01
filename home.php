56329852041fd4c8b5b0b659654884f7e25440b6
Sani <sanialamofficial@gmail.com>
2026-01-01 22:50:32 +0600

    homepage

---
+++ home.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+
+    <style>
+        body{
+            background-color: lightblue;
+        }
+  
+        nav {
+            background-color: black;  
+            width: 1300px;            
+            display: block;
+            padding: 15px 0;
+            margin: 0 auto;            
+        }
+
+        nav a {
+            display: inline-block;
+            color: white;              
+            text-decoration: none;
+            padding: 10px 20px;
+            margin: 0 10px;
+            font-size: 18px;
+        }
+
+        
+        nav a:hover {
+            background-color: gray;    
+            color: yellow;             
+            border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Menu</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+    </nav>
+
+</body>
+</html>
